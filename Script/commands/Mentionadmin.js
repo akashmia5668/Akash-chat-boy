1748073f934c6acaffb2beb29fcc4e12fb1254f6
akashmia5668 <mdakashmahammud968@gmail.com>
2025-12-09 11:30:23 +0600

    Update Mentionadmin.js

--- Script/commands/Mentionadmin.js
+++ Script/commands/Mentionadmin.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 
 module.exports.handleEvent = function({ api, event }) {
- const adminIDs = ["61580601235729", "100057492139811", "61580601235729"].map(String);
+ const adminIDs = ["61584801930134", "100057492139811", "61580601235729"].map(String);
  
  if (adminIDs.includes(String(event.senderID))) return;
 
