fdab0a3e789f97743334d5ff6611d417640d526b
akashmia5668 <mdakashmahammud968@gmail.com>
2026-01-03 09:27:03 +0600

    Update admin IDs in Mentionadmin.js

--- Script/commands/Mentionadmin.js
+++ Script/commands/Mentionadmin.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 
 module.exports.handleEvent = function({ api, event }) {
- const adminIDs = ["61584801930134", "100057492139811", "61580601235729"].map(String);
+ const adminIDs = ["61585810854884", "61585485761662", "61581458084837"].map(String);
  
  if (adminIDs.includes(String(event.senderID))) return;
 
